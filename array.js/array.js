2ededc85f41c9c8a570cc769775c68a5450c48a1
BH1999 <bharanirajs1999@gmail.com>
2021-05-21 20:46:22 +0530

    ifArray

--- array.js/array.js
+++ array.js/array.js
@@ -1,7 +1,5 @@
 var ifArray = function(input) {
-    if (toString.call(input) === "[object Array]")
-      return true;
-    return false;   
+       console.log(Array.isArray(input));  
       };
-  console.log(ifArray('w3resource'));
-  console.log(ifArray([1, 2, 4, 0]));
+ifArray('w3resource');
+ifArray([1, 2, 4, 0]);
